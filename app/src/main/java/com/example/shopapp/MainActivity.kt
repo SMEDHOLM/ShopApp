8a9444fd0d08aa07d9fbfa8e4a842e0e773f8f59
SMEDHOLM <smedholm.anton@gmail.com>
2026-01-20 15:46:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/shopapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.shopapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.service.autofill.UserData
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val userLogin: EditText = findViewById(R.id.user_login)
+        val userEmail: EditText = findViewById(R.id.user_email)
+        val userPass: EditText = findViewById(R.id.user_pass)
+        val button: Button = findViewById(R.id.button_reg)
+
+        button.setOnClickListener {
+            val login = userLogin.text.toString().trim()
+            val email = userEmail.text.toString().trim()
+            val pass = userPass.text.toString().trim()
+
+            if (login.isBlank() || email.isBlank() || pass.isBlank())
+                Toast.makeText(this, "Fill all fields", Toast.LENGTH_LONG).show()
+            else {
+                val user = User(login, email, pass)
+                val db = DbHelper(this, null)
+                db.addUser(user)
+                Toast.makeText(this, "User added", Toast.LENGTH_LONG).show()
+                userLogin.text.clear()
+                userEmail.text.clear()
+                userPass.text.clear()
+            }
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/shopapp/User.kt
@@ -0,0 +1,4 @@
+package com.example.shopapp
+
+class User(val login: String, val email: String,val pass: String,) {
+}
